34d4b6aee3c0203cae53655b4737f975f6c5e646
Abukarabukare <Abukarshegow@gmail.comm>
2024-03-15 18:25:55 -0400

    Complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,13 +4,42 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+            int forward = 0;
+            int backward = 0;
+            //Forward
+        for (int i = startIndex; i < playList.length; i++) {
+            if (selection.equals(playList[i])) {
+                 forward = i - startIndex;
+                break;
+
+            }
+        }
+        //backward
+        for (int j = startIndex; j >= 0 ; j--) {
+            if (selection.equals(playList[j])) {
+//                backward = startIndex - j;
+                break;
+            }
+            backward++;
+            //1
+            if (j == 0) {
+                //unclear
+                j = playList.length;
+            }
+
+        }
+       if (backward > forward){
+           return forward;
+       }
+       return backward;
     }
+
 }
+
 
 //d
